43e81d308eb430a05cb2dff5df70d482638f9485
Leila <nb.leila10@gmail.com>
2020-02-27 19:21:27 -0600

    Implement the product details screen

--- screens/shop/Product.js
+++ screens/shop/Product.js
@@ -2,14 +2,21 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { StyleSheet, Text, View, Image, Button, ScrollView } from "react-native";
 
+import Colors from "../../constants/Colors";
+
 const Product = props => {
   const prodId = props.navigation.getParam("productId");
   const selectedProd = useSelector(state => state.products.availableProducts.find(product => product.id === prodId));
 
   return (
-    <View>
-      <Text>The Product Details Screen: {selectedProd.title}</Text>
-    </View>
+    <ScrollView>
+      <Image style={styles.image} source={{ uri: selectedProd.imageUrl }} />
+      <View style={styles.actions}>
+        <Button color={Colors.primary} title='Add to Cart' onPress={() => {}} />
+      </View>
+      <Text style={styles.price}>${selectedProd.price.toFixed(2)}</Text>
+      <Text style={styles.description}>{selectedProd.description}</Text>
+    </ScrollView>
   );
 };
 
@@ -20,6 +27,26 @@ Product.navigationOptions = navData => {
   };
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 300
+  },
+  price: {
+    fontSize: 20,
+    color: "#888",
+    textAlign: "center",
+    marginVertical: 20
+  },
+  description: {
+    fontSize: 14,
+    textAlign: "center",
+    marginHorizontal: 20
+  },
+  actions: {
+    marginVertical: 10,
+    alignItems: "center"
+  }
+});
 
 export default Product;
