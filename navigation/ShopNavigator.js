50aa6911204a0daa0b6aa8d6c9be452c231abd9d
Leila <nb.leila10@gmail.com>
2020-02-27 19:53:39 -0600

    Use custom fonts

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -22,7 +22,13 @@ const ProductsNavigator = createStackNavigator(
       headerStyle: {
         backgroundColor: Platform.OS === "android" ? Colors.primary : ""
       },
-      headerTintColor: Platform.OS === "android" ? "white" : Colors.primary
+      headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
+      headerTitleStyle: {
+        fontFamily: "open-sans-bold"
+      },
+      headerBackTitleStyle: {
+        fontFamily: "open-sans"
+      }
     }
   }
 );
