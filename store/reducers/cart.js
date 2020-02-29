bf92b803186d656db4004767f4f4a6829f59b2dd
Leila <nb.leila10@gmail.com>
2020-02-29 12:02:01 -0600

    Clear cart when add order - click on Order button

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,4 +1,5 @@
 import { ADD_TO_CART, DELETE_FROM_CART } from "../actions/cart";
+import { ADD_ORDER } from "../actions/orders";
 import CartItem from "../../models/Cart-item";
 
 const initialState = {
@@ -41,6 +42,9 @@ export default cartReducer = (state = initialState, action) => {
           total: state.total - productPrice
         };
       }
+
+    case ADD_ORDER:
+      return initialState;
 
     default:
       return state;
