56d973817847e1ab65adf04e9518e3bc1335eecc
Leila <nb.leila10@gmail.com>
2020-03-04 20:01:47 -0600

    Add error handling for register

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -21,7 +21,13 @@ export const signup = (email, password) => {
       );
 
       if (!response.ok) {
-        throw new Error("Something went wrong!");
+        const errorResData = await response.json();
+        const errorId = errorResData.error.message;
+        let message = "Something went wrong!";
+        if (errorId === "EMAIL_EXISTS") {
+          message = "This email exists already!";
+        }
+        throw new Error(message);
       }
 
       const resData = await response.json();
